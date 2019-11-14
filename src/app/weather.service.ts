c2cc9cef88ab73a92c9c075231728731bc434daa
Malik Sajid <engr.maliksajidkhan@gmail.com>
2019-11-14 21:17:06 +0500

    ionic 4 wheather app

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+   headerOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json"
+    })
+  };
 
   constructor(private httpClient: HttpClient) {}
 
   getWeatherFromApi(city: string){
-    return this.httpClient.get(`https://api.apixu.com/v1/current.json?key=YOUR_APIXU_KEY=${city}`);
+    return this.httpClient.get(`http://api.weatherstack.com/current?access_key=90f2a232385fe254f872289537a963db&query=${city}`);
   }
 }
