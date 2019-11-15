0588c305c6030b38efa86da4de59d4e1ff9a2f2d
Malik Sajid <engr.maliksajidkhan@gmail.com>
2019-11-15 19:20:01 +0500

    ionic 4 code. cors error fixed

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+const { Device } = Plugins;
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-   headerOptions = {
+  headerOptions = {
     headers: new HttpHeaders({
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:8100"
     })
   };
 
   constructor(private httpClient: HttpClient) {}
+  device = null;
 
-  getWeatherFromApi(city: string){
-    return this.httpClient.get(`http://api.weatherstack.com/current?access_key=90f2a232385fe254f872289537a963db&query=${city}`);
+  getWeatherFromApi(city: string) {
+      return this.httpClient.get(
+        `http://api.weatherstack.com/current?access_key=90f2a232385fe254f872289537a963db&query=${city}`
+      );
   }
 }
